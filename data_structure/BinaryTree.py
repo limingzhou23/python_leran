79f1c044ca134ddef78abdb3086b477f2660fb99
limingzhou23 <850073458@qq.com>
2020-06-20 15:06:01 +0800

    二叉树学习

--- data_structure/BinaryTree.py
+++ data_structure/BinaryTree.py
@@ -257,3 +257,64 @@ def postevaluate(tree):
 fpexp = ['(','3','+','(','4','*','5',')',')']
 parseTree = buildParseTree(fpexp)
 print(evaluate(parseTree))
+
+
+################################
+################################
+#二叉堆的实现
+class BinHeap:
+    def __init__(self):
+        #0项无用 只是为了后续方便 子节点等于父节点2倍
+        self.heapList = [0]
+        self.currentSize = 0
+
+    def perUP(self,i):
+        while i // 2 > 0:
+            if self.heapList[i] < self.heapList[i//2]:
+                tmp = self.heapList[i//2]
+                self.heapList[i//2] = self.heapList[i]
+                self.heapList[i] = tmp
+            i = i //2
+
+    def insert(self,k):
+        self.heapList.append(k)
+        self.currentSize = self.currentSize + 1
+        self.perUP(self.currentSize)
+
+    def perDown(self,i):
+        while (i*2) <= self.currentSize:
+            mc = self.minChild(i)
+            if self.heapList[i] > self.heapList[mc]:
+                tmp = self.heapList[i]
+                self.heapList[i] = self.heapList[mc]
+                self.heapList[mc] = tmp
+            i = mc
+
+    def minChild(self,i):
+        if i * 2 + 1 > self.currentSize:
+            return i * 2
+        else:
+            if self.heapList[i * 2] < self.heapList[i * 2 + 1]:
+                return i * 2
+            else:
+                return i * 2 + 1
+
+    def delMin(self):
+        retval = self.heapList[1]
+        self.heapList[1] = self.heapList[self.currentSize]
+        self.currentSize = self.currentSize - 1
+        self.heapList.pop()
+        self.perDown(1)
+        return retval
+
+    #无序表生成堆 下沉是O(n)
+    def buildHeap(self,alist):
+        i = len(alist) // 2
+        self.currentSize = len(alist)
+        self.heapList = [0] + alist[:]
+        print(len(self.heapList),i)
+        while(i > 0):
+            print(self.heapList,i)
+            self.perDown(i)
+            i = i - 1
+        print(self.heapList,i)

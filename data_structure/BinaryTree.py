c2e99637b8ab1c8a0319dd92403173a3f95e00ce
limingzhou23 <850073458@qq.com>
2020-06-21 15:05:22 +0800

    二叉树学习

--- data_structure/BinaryTree.py
+++ data_structure/BinaryTree.py
@@ -191,6 +191,38 @@ def inorder(tree):
         print(tree.getRootVal())
         inorder(tree.getRightChild())
 
+#层序遍历
+def PrintFromTopToBottom(root):
+    ans = []
+    if root == None:
+        return ans
+    else:
+        q = [root]
+        while q:
+            node = q.pop(0)
+            ans.append(node.val)
+            if node.left:
+                q.append(node.left)
+            if node.right:
+                q.append(node.right)
+        return ans
+#层序遍历恢复
+def createtree(l):
+    if l[0]:
+        root = BinatryTree(l[0])
+        nodes = [root]
+        id = 1
+        while nodes and id < len(l):
+            node = nodes[0]  # 依次为每个节点分配子节点
+            node.left = BinatryTree(l[id]) if l[id] else None
+            nodes.append(node.left)
+            node.right = BinatryTree(l[id + 1]) if id < len(l) - 1 and l[id + 1] else None
+            nodes.append(node.right)
+            id += 2  # 每次取出两个节点
+            nodes.pop(0)
+        return root
+    else:
+        return None
 
 
 #######################################
